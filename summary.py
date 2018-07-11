6a4e649ed9ea94cc083ad2b9d4fb9910b62a4fc0
Ken Bedwell <ken@hiringsolved.com>
2018-07-10 22:45:20 -0400

    first

---
+++ summary.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+
+def days_between(d1, d2):
+    d, m, y = d1.split('/')
+    d1 = datetime.strptime('%s/%s/%s' % (d.zfill(2), m.zfill(2), y), "%m/%d/%Y")
+    d, m, y = d2.split('/')
+    d2 = datetime.strptime('%s/%s/%s' % (d.zfill(2), m.zfill(2), y), "%m/%d/%Y")
+    return abs((d2 - d1).days)
+
+
+def main(lines):
+    inv = {}
+    for l in [l.strip() for l in lines]:
+        (flavor, start, end) = l.split(',')
+        diff = days_between(start, end)
+        if flavor not in inv:
+            inv[flavor] = {
+                'cnt': 1,
+                'diff_sum': int(diff),
+                'max_diff': int(diff),
+                'min_diff': int(diff)
+            }
+        else:
+            inv[flavor]['cnt'] += 1
+            inv[flavor]['diff_sum'] += int(diff)
+            max_diff = inv[flavor]['max_diff']
+            if int(diff) > int(max_diff):
+                inv[flavor]['max_diff'] = diff
+            min_diff = inv[flavor]['min_diff']
+            if int(diff) < int(min_diff):
+                inv[flavor]['min_diff'] = diff
+
+    for flavor in inv:
+        dat = inv[flavor]
+        diff_sum = dat['diff_sum']
+        cnt = dat['cnt']
+        max_diff = dat['max_diff']
+        min_diff = dat['min_diff']
+        avg = round(float(diff_sum / cnt), 1)
+        print('{:<20},{:<20},{:>10},{:>13},{:>13}'.format(cnt, flavor, avg, max_diff, min_diff))
+
+
+if __name__ == '__main__':
+    with open('inventory.csv') as f:
+        lines = f.readlines()
+    main(lines)
